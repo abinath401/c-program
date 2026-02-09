06d5f0d32c92c6a663013f3d83aa2a1ea104c1be
Abinath R <abinathabi401@gmail.com>
2026-02-09 15:24:26 +0530

    Declare additional integer variables and print them

--- C practice-Declare two integer variable.c
+++ C practice-Declare two integer variable.c
@@ -3,7 +3,12 @@ int main()
 {
    int a =10;
    printf("%d \n",a);
-   int b =20;
+   int b =15;
    printf("%d",b);
+   int c=20;
+   printf("%d",c);
+   int d=40;
+   printf("%d",d);
    return 0;
+
 }
