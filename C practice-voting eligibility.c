ccde1d03fe9c54d68a73c4d6b82481c84381e35a
Abinath R <abinathabi401@gmail.com>
2026-02-12 10:13:00 +0530

    Add files via upload

---
+++ C practice-voting eligibility.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=18)
+    {
+        printf("elligble for voting");
+    }
+else{
+    printf(" not elligble for voting ");
+}
+return 0;
+}
